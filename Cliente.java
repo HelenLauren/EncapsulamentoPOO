ef16fc1a301c372e4f3e428fe734ddb17af331ee
Helen Lauren Bonato <167373993+HelenLauren@users.noreply.github.com>
2024-08-23 15:20:07 -0300

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,17 +1,47 @@
-import java.math.BigDecimal;
-
 public class Cliente {
     private String nome;
-    private String email;
     private String cpf;
+    private double rendaMensal;
     private String senha;
 
     public Cliente(String nome, String email, String senha, String cpf){
-        this.nome = nome;
-        this.email = email;
-        this.cpf = cpf;
-        this.senha = senha;
+        setNome(nome);
+        setCpf(cpf);
+        setRendaMensal(rendaMensal);
+        setSenha(senha);
 
+    }
+     public String getNome() {
+        return nome;
+    }
+     public void setNome(String nome) {
+        this.nome = nome.toUpperCase();
+    }
+     public String getCpf() {
+        return cpf;
+    }
+     public void setCpf(String cpf) {
+        this.cpf = cpf.replaceAll("(\\d{3})(\\d{3})(\\d{3})(\\d{2})", "$1.$2.$3-$4");
+    }
+     public double getRendaMensal() {
+        return rendaMensal;
+    }
+     public void setRendaMensal(double rendaMensal) {
+        if(rendaMensal <= 0) {
+            System.out.println("Renda mensal inválida! Deve ser maior que zero.");
+            return;
+        }
+        this.rendaMensal = rendaMensal;
+    }
+    public String getSenha(){
+        return senha;
+    }
+    public void setSenha(String senha){
+        if (senha != null && senha.matches("\\d{6}")) { 
+            this.senha = senha;
+        } else {
+            throw new IllegalArgumentException("Senha inválida. Deve conter 6 dígitos numéricos");
+        }
     }
 
 }
